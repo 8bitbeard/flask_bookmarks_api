c437d25edc4ce1699ed40c7dc3e1b4e0b7a74f2e
Wilton Souza <willsouzafilho@gmail.com>
2021-08-13 13:48:41 -0300

    Setting up Database and Models

--- src/__init__.py
+++ src/__init__.py
@@ -2,6 +2,7 @@ from flask import Flask
 
 from src.auth import auth
 from src.bookmarks import bookmarks
+from src.database.database import db
 
 import os
 
@@ -12,11 +13,14 @@ def create_app(test_config=None):
 
     if test_config is None:
         app.config.from_mapping(
-            SECRET_KEY=os.environ.get("SECRET_KEY")
+            SECRET_KEY=os.environ.get("SECRET_KEY"),
+            SQLALCHEMY_DATABASE_URI=os.environ.get("SQLALCHEMY_DATABASE_URI")
         )
     else:
         app.config.from_mapping(test_config)
 
+    db.app=app
+    db.init_app(app)
     app.register_blueprint(auth)
     app.register_blueprint(bookmarks)
 
