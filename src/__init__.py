b6e11b1b3b493cd7cc119206fa49e39f56478773
Wilton Souza <willsouzafilho@gmail.com>
2021-08-19 22:39:34 -0300

    Adjusting project folder

--- src/__init__.py
+++ src/__init__.py
@@ -1,7 +1,5 @@
 from flask import Flask
 from flask.json import jsonify
-from flask_migrate import Migrate
-
 
 from flask_jwt_extended import JWTManager
 
@@ -17,28 +15,11 @@ from src.config import config_by_name
 
 
 
-
-
-# def create_app(test_config=None):
-
-#     app = Flask(__name__, instance_relative_config=True)
-
-#     if test_config is None:
-#         app.config.from_mapping(
-#             SECRET_KEY=os.environ.get("SECRET_KEY"),
-#             SQLALCHEMY_DATABASE_URI=os.environ.get("SQLALCHEMY_DATABASE_URI"),
-#             SQLALCHEMY_TRACK_MODIFICATIONS=False,
-#             JWT_SECRET_TOKEN=os.environ.get('JWT_SECRET_KEY')
-#         )
-#     else:
-#         app.config.from_mapping(test_config)
-
 def create_app(config_name='development'):
 
     app = Flask(__name__)
     app.config.from_object(config_by_name[config_name])
 
-    migrate = Migrate(app, db)
     app.app_context().push()
     db.app=app
     db.init_app(app)
